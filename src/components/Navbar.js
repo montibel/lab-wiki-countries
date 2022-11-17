67b0df679abc7817a73e40c27dfa7dab526731d0
montibel <113550254+montibel@users.noreply.github.com>
2022-11-17 16:06:42 +0100

    navbar ready

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+/* <!-- Navbar --> */
+<nav className="navbar">
+  <div className="container">
+    <title>LAB - WikiCountries</title>
+    <a className="navbarBrand" href="/">
+      WikiCountries
+    </a>
+  </div>
+</nav>;
